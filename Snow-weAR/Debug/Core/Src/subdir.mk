a801a112d56adfeada779e8c38223c0b4e9aba4d
Peter <petersumner515@gmail.com>
2020-11-19 14:26:13 -0500

    add BB

--- Snow-weAR/Debug/Core/Src/subdir.mk
+++ Snow-weAR/Debug/Core/Src/subdir.mk
@@ -4,6 +4,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../Core/Src/BB.c \
 ../Core/Src/GPS.c \
 ../Core/Src/main.c \
 ../Core/Src/stm32l4xx_hal_msp.c \
@@ -13,6 +14,7 @@ C_SRCS += \
 ../Core/Src/usart.c 
 
 OBJS += \
+./Core/Src/BB.o \
 ./Core/Src/GPS.o \
 ./Core/Src/main.o \
 ./Core/Src/stm32l4xx_hal_msp.o \
@@ -22,6 +24,7 @@ OBJS += \
 ./Core/Src/usart.o 
 
 C_DEPS += \
+./Core/Src/BB.d \
 ./Core/Src/GPS.d \
 ./Core/Src/main.d \
 ./Core/Src/stm32l4xx_hal_msp.d \
